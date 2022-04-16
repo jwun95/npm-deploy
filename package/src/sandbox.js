78928a08ceab7bc65929e563b5ded85492c20794
jwun95 <jwun95@naver.com>
2022-04-16 17:30:23 +0900

    change

---
+++ package/src/sandbox.js
@@ -0,0 +1,5 @@
+import { test } from "./scroll.js";
+
+export default {
+  test,
+};
---
+++ package/src/scroll.js
@@ -0,0 +1,8 @@
+const scrollButton = document.querySelector(".scrollButton");
+const scrollContainer = document.querySelector(".container");
+
+export function changeColor() {
+  scrollButton.addEventListener("click", function (event) {
+    scrollContainer.style.backgroundColor = "red";
+  });
+}
